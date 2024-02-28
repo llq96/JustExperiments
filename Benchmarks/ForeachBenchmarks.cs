6ec493d080364178e9a0378e1e73bba737be778c
VladB <bovinov.vlad@gmail.com>
2024-02-29 00:04:02 +0300

    A lot of work

--- Benchmarks/ForeachBenchmarks.cs
+++ Benchmarks/ForeachBenchmarks.cs
@@ -1,26 +1,29 @@
 ﻿using BenchmarkDotNet.Attributes;
+using ExamplesForInterview.Extensions;
 
 namespace ExamplesForInterview;
 
 //Выводы:
-//Для статического метода генерируется отдельный статический класс с экшеном и каждый раз выполняется проверка на null
-//(Видимо)Из-за этого методы становятся в 3 раза медленнее
-
-// Последние результаты (CountElements = 100;):
-// | Method                   | Mean      | Error    | StdDev   | Ratio | RatioSD | Gen0   | Allocated | Alloc Ratio |
-// |------------------------- |----------:|---------:|---------:|------:|--------:|-------:|----------:|------------:|
-// | ArrayFor                 |  27.02 ns | 0.340 ns | 0.318 ns |  1.00 |    0.00 |      - |         - |          NA |
-// | ArrayForeach             |  25.54 ns | 0.351 ns | 0.311 ns |  0.94 |    0.02 |      - |         - |          NA |
-// | ListFor                  |  37.79 ns | 0.766 ns | 1.632 ns |  1.41 |    0.04 |      - |         - |          NA |
-// | ListForeach              |  36.19 ns | 0.434 ns | 0.406 ns |  1.34 |    0.02 |      - |         - |          NA |
-// | ListForeachMethod        | 184.26 ns | 2.264 ns | 2.118 ns |  6.82 |    0.13 |      - |         - |          NA |
-// | ArrayToListForeachMethod | 230.56 ns | 3.568 ns | 3.337 ns |  8.53 |    0.19 | 0.0544 |     456 B |          NA |
-// | ListMyForeachMethod      | 165.86 ns | 1.468 ns | 1.373 ns |  6.14 |    0.09 |      - |         - |          NA |
-// | ListMyForeach2Method     | 185.44 ns | 3.313 ns | 3.099 ns |  6.86 |    0.12 |      - |         - |          NA |
+// Для статического метода генерируется отдельный статический класс с экшеном и каждый раз выполняется проверка на null
+// (Видимо)Из-за этого методы становятся в 3 раза медленнее
+// Кэшировать размер коллекции выгодно
 // ArrayForeach чуть быстрее, видимо потому что обращение к стандартному массиву медленнее
 // ListFor медленнее из-за индексаторов
 // ListForEach медленнее из-за try блока и постоянных MoveNext() и Current
 // Варианты с передачей экшена медленнее видимо из-за самого экшена
+// Простой вариант через рефлексию в 2.5 раза медленнее, и даже с разбиением на 4 всё равно в 1.5 раза медленнее
+// А вот продвинутый вариант с взятием поля подходит
+
+// Последние результаты (CountElements = 100;):
+// | Method                              | Mean     | Error    | StdDev   | Ratio | RatioSD | Gen0   | Allocated | Alloc Ratio |
+// |------------------------------------ |---------:|---------:|---------:|------:|--------:|-------:|----------:|------------:|
+// | ArrayFor                            | 27.00 ns | 0.523 ns | 0.489 ns |  1.00 |    0.00 |      - |         - |          NA |
+// | ArrayFor_Try4_2                     | 15.37 ns | 0.319 ns | 0.327 ns |  0.57 |    0.02 |      - |         - |          NA |
+// | Act                                 | 46.67 ns | 0.694 ns | 0.649 ns |  1.73 |    0.04 | 0.0076 |      64 B |          NA |
+// | Act_Try8                            | 38.97 ns | 0.304 ns | 0.285 ns |  1.44 |    0.03 | 0.0076 |      64 B |          NA |
+// | ActWithReflection_Try4              | 11.48 ns | 0.248 ns | 0.244 ns |  0.43 |    0.01 | 0.0076 |      64 B |          NA |
+// | ActWithReflection_MyReflection      | 71.67 ns | 1.309 ns | 1.225 ns |  2.66 |    0.07 | 0.0076 |      64 B |          NA |
+// | ActWithReflection_Try4_MyReflection | 41.23 ns | 0.845 ns | 1.434 ns |  1.51 |    0.04 | 0.0076 |      64 B |          NA |
 
 // Последние результаты (CountElements = 100000;):
 // | Method                   | Mean      | Error    | StdDev   | Ratio | RatioSD | Gen0     | Gen1     | Gen2     | Allocated | Alloc Ratio |
@@ -37,7 +40,7 @@ namespace ExamplesForInterview;
 [MemoryDiagnoser]
 public class ForeachBenchmarks
 {
-    private const int CountElements = 100000;
+    private const int CountElements = 100;
     private static readonly int[] TestArray = Enumerable.Range(0, CountElements).Select(i => i).ToArray();
     private static readonly List<int> TestList = Enumerable.Range(0, CountElements).Select(i => i).ToList();
 
@@ -50,96 +53,66 @@ public class ForeachBenchmarks
         }
     }
 
-    [Benchmark]
-    public void ArrayForeach()
-    {
-        foreach (var number in TestArray)
-        {
-            SomeAction(number);
-        }
-    }
-
-    [Benchmark]
-    public void ListFor()
-    {
-        for (var i = 0; i < TestList.Count; i++)
-        {
-            SomeAction(TestList[i]);
-        }
-    }
-
-    [Benchmark]
-    public void ListForeach()
-    {
-        foreach (var number in TestList)
-        {
-            SomeAction(number);
-        }
-    }
-
-    [Benchmark]
-    public void ListForeachMethod()
-    {
-        TestList.ForEach(SomeAction);
-    }
-
-    [Benchmark]
-    public void ArrayToListForeachMethod()
-    {
-        TestArray.ToList().ForEach(SomeAction);
-    }
-
-    [Benchmark]
-    public void ListMyForeachMethod()
-    {
-        TestList.MyForEach(SomeAction);
-    }
-
-    [Benchmark]
-    public void ListMyForeach2Method()
-    {
-        TestList.MyForEach2(SomeAction);
-    }
-
     // [Benchmark]
-    // public void ArrayMyForeach3Method()
+    // public void ArrayForeach()
     // {
-    //     TestArray.MyForEach3(SomeAction);
-    // }
-
-    // [Benchmark]
-    // public unsafe void ArrayMyForeach4Method()
-    // {
-    //     MyForEach4(TestArray, &SomeAction);
-    // }
-    //
-    // private static unsafe void MyForEach4(int[] array, delegate*<int, void> action)
-    // {
-    //     foreach (var element in array)
+    //     foreach (var number in TestArray)
     //     {
-    //         action(element);
+    //         SomeAction(number);
     //     }
     // }
 
-    // [Benchmark]
-    // public unsafe void ArrayUnsafeFor()
-    // {
-    //     var count = TestArray.Length;
-    //     fixed (int* ptr = TestArray)
-    //         for (int i = 0; i < count; i++)
-    //         {
-    //             SomeAction(*(ptr + i));
-    //         }
-    // }
+    [Benchmark]
+    public void ArrayFor_Try4_2()
+    {
+        var count = TestArray.Length;
+        for (var i = 0; i <= count - 4; i += 4)
+        {
+            SomeAction(TestArray[i]);
+            SomeAction(TestArray[i + 1]);
+            SomeAction(TestArray[i + 2]);
+            SomeAction(TestArray[i + 3]);
+        }
+    }
 
-    private static void SomeAction(int num)
+    [Benchmark]
+    public void Act()
+    {
+        TestList.Act(SomeAction);
+    }
+
+    [Benchmark]
+    public void Act_Try8()
+    {
+        TestList.Act_Try8(SomeAction);
+    }
+
+    [Benchmark]
+    public void ActWithReflection_Try4()
+    {
+        TestList.ActWithReflection_Try4(SomeAction);
+    }
+
+    [Benchmark]
+    public void ActWithReflection_MyReflection()
+    {
+        TestList.ActWithReflection_MyReflection(SomeAction);
+    }
+
+    [Benchmark]
+    public void ActWithReflection_Try4_MyReflection()
+    {
+        TestList.ActWithReflection_Try4_MyReflection(SomeAction);
+    }
+
+    private void SomeAction(int num)
     {
     }
 }
 
 public static class TestExtensions
 {
-    public static void MyForEach<T>(this List<T> list, Action<T> action)
+    public static void Act<T>(this List<T> list, Action<T> action)
     {
         var count = list.Count;
         for (int i = 0; i < count; i++)
@@ -148,49 +121,55 @@ public static class TestExtensions
         }
     }
 
-    public static void MyForEach2<T>(this List<T> list, Action<T> action)
+    public static void Act_Try8<T>(this List<T> list, Action<T> action)
     {
-        using List<T>.Enumerator enumerator = list.GetEnumerator();
-
-        while (enumerator.MoveNext())
+        var count = list.Count;
+        for (int i = 0; i <= count - 8; i += 8)
         {
-            action.Invoke(enumerator.Current);
+            action.Invoke(list[i]);
+            action.Invoke(list[i + 1]);
+            action.Invoke(list[i + 2]);
+            action.Invoke(list[i + 3]);
+            action.Invoke(list[i + 4]);
+            action.Invoke(list[i + 5]);
+            action.Invoke(list[i + 6]);
+            action.Invoke(list[i + 7]);
         }
     }
 
-    // public static unsafe void MyForEach3<T>(this T[] array, Action<T> action)
-    // {
-    //     var count = array.Length;
-    //     fixed (T* ptr = array)
-    //         for (int i = 0; i < count; i++)
-    //         {
-    //             action.Invoke(*(ptr + i));
-    //         }
-    // }
+    public static void ActWithReflection_Try4<T>(this List<T> list, Action<T> action)
+    {
+        var array = list.GetInternalArray();
+        var count = array.Length;
+        for (int i = 0; i <= count - 4; i += 4)
+        {
+            action.Invoke(array[i]);
+            action.Invoke(array[i + 1]);
+            action.Invoke(array[i + 2]);
+            action.Invoke(array[i + 3]);
+        }
+    }
 
-    // public static unsafe void MyForEach4<T>(this T[] array, delegate*<T, void> action)
-    // {
-    //     foreach (var element in array)
-    //     {
-    //         action(element);
-    //     }
-    //     // var count = array.Length;
-    //     // fixed (T* ptr = array)
-    //     //     for (int i = 0; i < count; i++)
-    //     //     {
-    //     //         action(default);
-    //     //         // action(*(ptr + i));
-    //     //         // action?.Invoke(*(ptr + i));
-    //     //     }
-    // }
+    public static void ActWithReflection_MyReflection<T>(this List<T> list, Action<T> action)
+    {
+        var array = list.GetInternalArray2();
+        var count = array.Length;
+        for (int i = 0; i < count; i++)
+        {
+            action.Invoke(array[i]);
+        }
+    }
 
-    // public static unsafe void MyForEach4(this object[] array, Action<int>* action)
-    // {
-    //     var count = array.Length;
-    //     fixed (object* ptr = array)
-    //         for (int i = 0; i < count; i++)
-    //         {
-    //             (*action)?.Invoke((int)(*(ptr + i)));
-    //         }
-    // }
+    public static void ActWithReflection_Try4_MyReflection<T>(this List<T> list, Action<T> action)
+    {
+        var array = list.GetInternalArray2();
+        var count = array.Length;
+        for (int i = 0; i <= count - 4; i += 4)
+        {
+            action.Invoke(array[i]);
+            action.Invoke(array[i + 1]);
+            action.Invoke(array[i + 2]);
+            action.Invoke(array[i + 3]);
+        }
+    }
 }
