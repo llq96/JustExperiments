4af63be58a82cb20338564c73db9ba654c7afa62
VladB <bovinov.vlad@gmail.com>
2024-02-29 01:46:10 +0300

    Great 2

--- Benchmarks/ForeachBenchmarks.cs
+++ Benchmarks/ForeachBenchmarks.cs
@@ -13,22 +13,18 @@ namespace ExamplesForInterview;
 // Варианты с передачей экшена медленнее видимо из-за самого экшена
 // Простой вариант через рефлексию в 2.5 раза медленнее, и даже с разбиением на 4 всё равно в 1.5 раза медленнее
 // А вот продвинутый вариант с взятием поля подходит, без фишек всего в 1.2 раза медленнее
+// В два раза быстрее цикла for (!)
 
-// Последние результаты (CountElements = 100;):
-// | Method                              | Mean     | Error    | StdDev   | Ratio | RatioSD | Gen0   | Allocated | Alloc Ratio |
-// |------------------------------------ |---------:|---------:|---------:|------:|--------:|-------:|----------:|------------:|
-// | ArrayFor                            | 27.00 ns | 0.523 ns | 0.489 ns |  1.00 |    0.00 |      - |         - |          NA |
-// | ArrayFor_Try4_2                     | 15.37 ns | 0.319 ns | 0.327 ns |  0.57 |    0.02 |      - |         - |          NA |
-// | Act                                 | 46.67 ns | 0.694 ns | 0.649 ns |  1.73 |    0.04 | 0.0076 |      64 B |          NA |
-// | Act_Try8                            | 38.97 ns | 0.304 ns | 0.285 ns |  1.44 |    0.03 | 0.0076 |      64 B |          NA |
-// | ActWithReflection_Try4              | 11.48 ns | 0.248 ns | 0.244 ns |  0.43 |    0.01 | 0.0076 |      64 B |          NA |
-// | ActWithReflection_MyReflection      | 71.67 ns | 1.309 ns | 1.225 ns |  2.66 |    0.07 | 0.0076 |      64 B |          NA |
-// | ActWithReflection_Try4_MyReflection | 41.23 ns | 0.845 ns | 1.434 ns |  1.51 |    0.04 | 0.0076 |      64 B |          NA |
+// Последние результаты (CountElements = 103;):
+// | Method   | Mean     | Error    | StdDev   | Ratio | Gen0   | Allocated | Alloc Ratio |
+// |--------- |---------:|---------:|---------:|------:|-------:|----------:|------------:|
+// | ArrayFor | 27.15 ns | 0.340 ns | 0.266 ns |  1.00 |      - |         - |          NA |
+// | Act      | 13.55 ns | 0.292 ns | 0.259 ns |  0.50 | 0.0076 |      64 B |          NA |
 
 [MemoryDiagnoser]
 public class ForeachBenchmarks
 {
-    private const int CountElements = 128;
+    private const int CountElements = 103;
     private static readonly int[] TestArray = Enumerable.Range(0, CountElements).Select(i => i).ToArray();
     private static readonly List<int> TestList = Enumerable.Range(0, CountElements).Select(i => i).ToList();
 
@@ -41,22 +37,10 @@ public class ForeachBenchmarks
         }
     }
 
-    // [Benchmark]
-    // public void Act_Try4()
-    // {
-    //     TestList.Act_Try4(SomeAction);
-    // }
-
     [Benchmark]
     public void Act()
     {
         TestList.Act(SomeAction);
-    }
-
-    [Benchmark]
-    public void Act_Try4_Safe()
-    {
-        TestList.Act_Try4_Safe(SomeAction);
     }
 
     private void SomeAction(int num)
@@ -67,16 +51,6 @@ public class ForeachBenchmarks
 public static class TestExtensions
 {
     public static void Act<T>(this List<T> list, Action<T> action)
-    {
-        var array = list.GetInternalArray();
-        var count = list.Count;
-        for (int i = 0; i < count; i++)
-        {
-            action.Invoke(array[i]);
-        }
-    }
-
-    public static void Act_Try4_Safe<T>(this List<T> list, Action<T> action)
     {
         var array = list.GetInternalArray();
         var rest = list.Count % 4;
