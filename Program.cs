3d28df6bb72e4a0d60e18e0d87bf9dd2698cb19f
VladB <bovinov.vlad@gmail.com>
2024-04-04 16:43:17 +0300

    remove old

--- Program.cs
+++ Program.cs
@@ -6,46 +6,7 @@ internal class Program
 {
     private static void Main()
     {
-        // Console.WriteLine("Hello World!");
-
-        // new BoxingAndCasting().Run();
-        // new ExceptionTests().Run();
-        // new FinallyReturnTests().Run();
-        // new EnumFlagTests().Run();
-        // new InterfaceEnumerableTests().Run();
-        // new AsyncTests().Run();
-        // new GoodNumberSwitchTests().Run();
-        // new MyBenchmarks().Run();
-        // new ClosureILCodeTests().Run();
-
-        // BenchmarkRunner.Run<MyBenchmarks>();
-        // BenchmarkRunner.Run<ForeachBenchmarks>();
-
-        // new ForeachBenchmarks().Act_Try4();
-        // new ForeachBenchmarks().List_32Test_AsSpan();
-
-        // for (int i = 0; i < 10; i++)
-        // {
-        //     List<int> test = Enumerable.Range(0, i).Select(x => x).ToList();
-        //     Console.WriteLine();
-        //     test.Act(Console.Write);
-        //     Console.WriteLine();
-        //     test.SimpleAct(Console.Write);
-        // }
-
-        // Series.Run();
-        // new ForEachILCodeTests().Run();
-
-        // BenchmarkRunner.Run<SplitBenchmarks>();
-        // new SplitBenchmarks().Check();
-
-        // new InterfacesILCodeTests().Run();
-        // new AnimalsExample().Example5();
-        // new AsyncTests2().Run();
-        // new ReactivePropertyTests().Run();
-        // new RefOutIn_InMethods().Run();
-        // new ConstructorViaReflection().Run();
         // BenchmarkRunner.Run<InParameterBenchmark>();
-        new StringsEquals().Run();
+        // new StringsEquals().Run();
     }
 }
