c3233266a77dcd2b45532dfb1b312a4a9b39b8d4
VladB <bovinov.vlad@gmail.com>
2024-05-23 23:15:36 +0300

    Improve ExceptionTests.cs

--- ExceptionTests.cs
+++ ExceptionTests.cs
@@ -1,35 +1,60 @@
 ﻿namespace ExamplesForInterview;
 
+// Experiment Throw:
+// System.Exception: Exception of type 'System.Exception' was thrown.
+//    at ExamplesForInterview.ExceptionTests.<MethodWithException>g__D|3_3() in C:\Users\Vlad\RiderProjects\ExamplesForInterview\ExceptionTests.cs:line 53     
+//    at ExamplesForInterview.ExceptionTests.<MethodWithException>g__C|3_2() in C:\Users\Vlad\RiderProjects\ExamplesForInterview\ExceptionTests.cs:line 52     
+//    at ExamplesForInterview.ExceptionTests.<MethodWithException>g__B|3_1() in C:\Users\Vlad\RiderProjects\ExamplesForInterview\ExceptionTests.cs:line 51     
+//    at ExamplesForInterview.ExceptionTests.<MethodWithException>g__A|3_0() in C:\Users\Vlad\RiderProjects\ExamplesForInterview\ExceptionTests.cs:line 50     
+//    at ExamplesForInterview.ExceptionTests.MethodWithException() in C:\Users\Vlad\RiderProjects\ExamplesForInterview\ExceptionTests.cs:line 48
+//    at ExamplesForInterview.ExceptionTests.MethodWithThrow(ThrowType throwType) in C:\Users\Vlad\RiderProjects\ExamplesForInterview\ExceptionTests.cs:line 30
+//    at ExamplesForInterview.ExceptionTests.RunExperiment(ThrowType throwType) in C:\Users\Vlad\RiderProjects\ExamplesForInterview\ExceptionTests.cs:line 16  
+//
+// Experiment ThrowEx:
+// System.Exception: Exception of type 'System.Exception' was thrown.
+//    at ExamplesForInterview.ExceptionTests.MethodWithThrow(ThrowType throwType) in C:\Users\Vlad\RiderProjects\ExamplesForInterview\ExceptionTests.cs:line 39
+//    at ExamplesForInterview.ExceptionTests.RunExperiment(ThrowType throwType) in C:\Users\Vlad\RiderProjects\ExamplesForInterview\ExceptionTests.cs:line 16
+
 public class ExceptionTests
 {
     public void Run()
     {
-        Console.WriteLine($"Test {nameof(ThrowTestType.Throw)}:");
-        TestThrow(ThrowTestType.Throw);
-        Console.WriteLine();
-        Console.WriteLine($"Test {nameof(ThrowTestType.ThrowEx)}:");
-        TestThrow(ThrowTestType.ThrowEx);
+        RunExperiment(ThrowType.Throw);
+        RunExperiment(ThrowType.ThrowEx);
     }
 
-    private void TestThrow(ThrowTestType throwTestType)
+    private void RunExperiment(ThrowType throwType)
     {
+        Console.WriteLine($"Experiment {throwType}:");
         try
         {
-            switch (throwTestType)
-            {
-                case ThrowTestType.Throw:
-                    MethodWithThrowInCatch();
-                    break;
-                case ThrowTestType.ThrowEx:
-                    MethodWithThrowExceptionInCatch();
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(throwTestType), throwTestType, null);
-            }
+            MethodWithThrow(throwType);
         }
         catch (Exception e)
         {
             Console.WriteLine(e);
+        }
+
+        Console.WriteLine();
+    }
+
+    private static void MethodWithThrow(ThrowType throwType)
+    {
+        try
+        {
+            MethodWithException();
+        }
+        catch (Exception e)
+        {
+            switch (throwType)
+            {
+                case ThrowType.Throw:
+                    throw;
+                case ThrowType.ThrowEx:
+                    throw e;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(throwType), throwType, null);
+            }
         }
     }
 
@@ -43,39 +68,7 @@ public class ExceptionTests
         void D() => throw new Exception();
     }
 
-    private static void MethodWithThrowInCatch()
-    {
-        try
-        {
-            MethodWithException();
-        }
-        catch (Exception e)
-        {
-            throw;
-        }
-        finally
-        {
-            // Console.WriteLine($"{nameof(MethodWithThrowInCatch)} Finally");
-        }
-    }
-
-    private static void MethodWithThrowExceptionInCatch()
-    {
-        try
-        {
-            MethodWithException();
-        }
-        catch (Exception ex)
-        {
-            throw ex;
-        }
-        finally
-        {
-            // Console.WriteLine($"{nameof(MethodWithThrowExceptionInCatch)} Finally");
-        }
-    }
-
-    private enum ThrowTestType
+    private enum ThrowType
     {
         Throw,
         ThrowEx
