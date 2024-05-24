80b366320762de375f5ccc4b4435fc7a0a19bb98
VladB <bovinov.vlad@gmail.com>
2024-05-24 12:32:44 +0300

    Add idea

--- Ideas/Ideas.cs
+++ Ideas/Ideas.cs
@@ -2,76 +2,92 @@
 
 public class Ideas
 {
-    //--------------------------------------   XmlSerializer не сериализует hashtable, но в остальных случаях сериализация работает
+//--------------------------------------   В foreach классы под делегат создаются для каждого элемента
 
-    // var t = new Hashtable();
-    // t.Add("Hi!", "I'm here");
-    // t.Add("Hm", "Yup");
-    //
-    // var serializer = new XmlSerializer(typeof(Hashtable));
-    //
-    // using (var sw = new StringWriter())
-    // {
-    //     serializer.Serialize(sw, t);
-    //
-    //     Console.WriteLine(sw.ToString());
-    // }
+// List<int> list = new() { 1, 2, 3 };
+// List<Action> actions = new();
+// for (int i = 0; i<list.Count;i++)
+// {
+//     actions.Add(() => Console.Write(i));
+// }
+//
+// foreach (var i in list)
+// {
+//     actions.Add(() => Console.Write(i));
+// }
+//
+// actions.ForEach(x => x.Invoke());
 
-    // Hashtable hashtable = new();
-    // hashtable.Add(1, 10);
-    // hashtable.Add(2, 20);
-    // hashtable.Add(3, 30);
-    // var json = JsonConvert.SerializeObject(hashtable);
-    // Console.WriteLine(json);
-    // var hashtable2 = JsonConvert.DeserializeObject<Hashtable>(json);
-    // Console.WriteLine(hashtable2.Count);
-    // foreach (DictionaryEntry entry in hashtable2)
-    // {
-    //     Console.WriteLine($"{entry.Value} {entry.Key}");
-    // }
+//--------------------------------------   XmlSerializer не сериализует hashtable, но в остальных случаях сериализация работает
 
-    // Dictionary<int, int> dictionary = new();
-    // dictionary.Add(1, 10);
-    // dictionary.Add(2, 20);
-    // dictionary.Add(3, 30);
-    // var json = JsonConvert.SerializeObject(dictionary);
-    // Console.WriteLine(json);
-    // var dictionary2 = JsonConvert.DeserializeObject<Dictionary<int, int>>(json);
-    // Console.WriteLine(dictionary2[1]);
-    // Console.WriteLine(dictionary2[2]);
-    // Console.WriteLine(dictionary2[3]);
+// var t = new Hashtable();
+// t.Add("Hi!", "I'm here");
+// t.Add("Hm", "Yup");
+//
+// var serializer = new XmlSerializer(typeof(Hashtable));
+//
+// using (var sw = new StringWriter())
+// {
+//     serializer.Serialize(sw, t);
+//
+//     Console.WriteLine(sw.ToString());
+// }
 
-    //-------------------------------------- Неочевидно, что такое есть
+// Hashtable hashtable = new();
+// hashtable.Add(1, 10);
+// hashtable.Add(2, 20);
+// hashtable.Add(3, 30);
+// var json = JsonConvert.SerializeObject(hashtable);
+// Console.WriteLine(json);
+// var hashtable2 = JsonConvert.DeserializeObject<Hashtable>(json);
+// Console.WriteLine(hashtable2.Count);
+// foreach (DictionaryEntry entry in hashtable2)
+// {
+//     Console.WriteLine($"{entry.Value} {entry.Key}");
+// }
 
-    // CancellationTokenSource.CreateLinkedTokenSource //
+// Dictionary<int, int> dictionary = new();
+// dictionary.Add(1, 10);
+// dictionary.Add(2, 20);
+// dictionary.Add(3, 30);
+// var json = JsonConvert.SerializeObject(dictionary);
+// Console.WriteLine(json);
+// var dictionary2 = JsonConvert.DeserializeObject<Dictionary<int, int>>(json);
+// Console.WriteLine(dictionary2[1]);
+// Console.WriteLine(dictionary2[2]);
+// Console.WriteLine(dictionary2[3]);
 
-    //-------------------------------------- Поэкспериментировать с try
+//-------------------------------------- Неочевидно, что такое есть
 
-    // ThreadPool.QueueUserWorkItem((obj) =>
-    // {
-    //     Console.WriteLine(obj.GetType());
-    //     throw new Exception("QWe");
-    // }, new bool());
-    // CancellationTokenSource source = new();
-    //
-    // Thread.Sleep(1000);
-    // Console.WriteLine("End");
+// CancellationTokenSource.CreateLinkedTokenSource //
 
-    //-------------------------------------- Разница в ответах при разных типах второго дженерика
+//-------------------------------------- Поэкспериментировать с try
 
-    // private static void Main()
-    // {
-    //     SomeClass1.Value = 1;
-    //     SomeClass2.Value = 2;
-    //     Console.WriteLine(SomeClass1.Value);
-    //     Console.WriteLine(SomeClass2.Value);
-    // }
-    //
-    // //
-    // public class SomeClass<T>
-    // {
-    //     public static int Value;
-    // }
-    // public class SomeClass1 : SomeClass<int>;
-    // public class SomeClass2 : SomeClass<int>;
+// ThreadPool.QueueUserWorkItem((obj) =>
+// {
+//     Console.WriteLine(obj.GetType());
+//     throw new Exception("QWe");
+// }, new bool());
+// CancellationTokenSource source = new();
+//
+// Thread.Sleep(1000);
+// Console.WriteLine("End");
+
+//-------------------------------------- Разница в ответах при разных типах второго дженерика
+
+// private static void Main()
+// {
+//     SomeClass1.Value = 1;
+//     SomeClass2.Value = 2;
+//     Console.WriteLine(SomeClass1.Value);
+//     Console.WriteLine(SomeClass2.Value);
+// }
+//
+// //
+// public class SomeClass<T>
+// {
+//     public static int Value;
+// }
+// public class SomeClass1 : SomeClass<int>;
+// public class SomeClass2 : SomeClass<int>;
 }
