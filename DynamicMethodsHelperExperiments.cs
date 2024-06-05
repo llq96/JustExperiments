7ac8a1f19e96a871da06b1ad91d1e27dccc89b37
VladB <bovinov.vlad@gmail.com>
2024-06-06 00:52:19 +0300

    Fixes, more experiments

--- DynamicMethodsHelperExperiments.cs
+++ DynamicMethodsHelperExperiments.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection.Emit;
-using DynamicMethodsHelpers;
+using ILDelegateHelpers;
 
 namespace ExamplesForInterview;
 
@@ -7,11 +7,24 @@ public class DynamicMethodsHelperExperiments
 {
     public void Run()
     {
-        var action = DynamicMethodsHelper.CreateActionMethod<int>((gen) =>
+        IntActionExperiment();
+        IntFuncExperiment();
+    }
+
+    private static void IntActionExperiment()
+    {
+        var action = ILDelegate.CreateAction<int>(gen =>
         {
             gen.Emit(OpCodes.Ldarg_0);
             gen.EmitCall(OpCodes.Call, typeof(Console).GetMethod("WriteLine", new[] { typeof(int) }), null);
         });
         action(456);
+    }
+
+    private static void IntFuncExperiment()
+    {
+        var func = ILDelegate.CreateFunc<int>(gen => { gen.Emit(OpCodes.Ldc_I4, 777); });
+        var result = func();
+        Console.WriteLine(result);
     }
 }
