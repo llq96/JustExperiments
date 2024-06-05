47f688ed3bd6ce2712ea75290428e71ccba857a1
VladB <bovinov.vlad@gmail.com>
2024-06-05 22:58:47 +0300

    Add DynamicMethodsHelperExperiments

---
+++ DynamicMethodsHelperExperiments.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection.Emit;
+using DynamicMethodsHelpers;
+
+namespace ExamplesForInterview;
+
+public class DynamicMethodsHelperExperiments
+{
+    public void Run()
+    {
+        var action = DynamicMethodsHelper.CreateActionMethod<int>((gen) =>
+        {
+            gen.Emit(OpCodes.Ldarg_0);
+            gen.EmitCall(OpCodes.Call, typeof(Console).GetMethod("WriteLine", new[] { typeof(int) }), null);
+        });
+        action(456);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,10 @@
-﻿using ExamplesForInterview.Examples;
-
-namespace ExamplesForInterview;
+﻿namespace ExamplesForInterview;
 
 internal class Program
 {
     private static void Main()
     {
         // BenchmarkRunner.Run<ForeachBenchmarks>();
-        new VirtualInvokes().Run();
+        new DynamicMethodsHelperExperiments().Run();
     }
 }
