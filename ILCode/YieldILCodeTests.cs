9bb26f32335bb3e64aa271fec029d1276d4b7562
VladB <bovinov.vlad@gmail.com>
2024-06-15 13:29:22 +0300

    Small improve

--- ILCode/YieldILCodeTests.cs
+++ ILCode/YieldILCodeTests.cs
@@ -1,11 +1,21 @@
 ﻿namespace ExamplesForInterview;
 
+/// <summary>
+/// Finaly вызывается в последнем стейте и в Dispose
+/// </summary>
 public class YieldILCodeTests
 {
     public IEnumerable<int> Get()
     {
-        yield return 0;
-        yield return 1;
-        yield return 2;
+        try
+        {
+            yield return 0;
+            yield return 1;
+            yield return 2;
+        }
+        finally
+        {
+            Console.WriteLine("Log");
+        }
     }
 }
