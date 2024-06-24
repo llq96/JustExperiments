a9b709dcb16c0a0e9bb98c165cc776dc9101faf1
VladB <bovinov.vlad@gmail.com>
2024-06-24 12:39:57 +0300

    Add directory for Telegram exercises

---
+++ MyTgWithExercises/Enum_001.cs
@@ -0,0 +1,16 @@
+﻿// public class Enum_001
+// {
+//     public static void Main()
+//     {
+//         var value = Enum.FirstFlag | Enum.ThirdFlag;
+//         System.Console.WriteLine(value);
+//     }
+//
+//     [Flags]
+//     private enum Enum
+//     {
+//         FirstFlag = 1,
+//         SecondFlag = 2,
+//         ThirdFlag = 4
+//     }
+// }
---
+++ MyTgWithExercises/Returns_002.cs
@@ -0,0 +1,26 @@
+﻿// public class Returns_002
+// {
+//     public static void Main()
+//     {
+//         var number = GetNumber();
+//         Console.WriteLine(number);
+//     }
+//
+//     private static int GetNumber()
+//     {
+//         try
+//         {
+//             return 1;
+//         }
+//         catch (Exception e)
+//         {
+//             return 2;
+//         }
+//         finally
+//         {
+//             return 3;
+//         }
+//
+//         return 4;
+//     }
+// }
