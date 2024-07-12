3be23022a4fd064b1f53c8b5a0ba565b58ca4779
VladB <bovinov.vlad@gmail.com>
2024-07-12 13:38:37 +0300

    More Exercises

--- Root/MyTgWithExercises/012.cs
+++ Root/MyTgWithExercises/012.cs
@@ -1,6 +1,15 @@
-﻿public class E_012
-{
-    public static void Main()
-    {
-    }
-}
+﻿// public class Params_012
+// {
+//     public static void Main()
+//     {
+//         var array = new int[] { 1, 2, 3 };
+//         Log(array);
+//         Log(4, 5, 6);
+//     }
+//
+//     public static void Log(params int[] array)
+//     {
+//         var str = string.Join(' ', array);
+//         Console.Write(str + ' ');
+//     }
+// }
