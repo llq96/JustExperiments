c7fb8929148e2b149968596ad0417c040efd0168
VladB <bovinov.vlad@gmail.com>
2024-07-26 17:43:09 +0300

    Add 16,17 example

---
+++ Root/MyTgWithExercises/016.cs
@@ -0,0 +1,19 @@
+﻿// public class Array_016
+// {
+//     private static void Main()
+//     {
+//         var array = new[] { 1, 2, 3 };
+//
+//         for (var i = 0; i < array.Length; i++)
+//         {
+//             Change(ref array[i]);
+//         }
+//
+//         Console.Write(string.Join(' ', array));
+//     }
+//
+//     private static void Change(ref int value)
+//     {
+//         value *= 2;
+//     }
+// }
---
+++ Root/MyTgWithExercises/017.cs
@@ -0,0 +1,19 @@
+﻿// public class List_017
+// {
+//     private static void Main()
+//     {
+//         var list = new List<int> { 1, 2, 3 };
+//
+//         for (var i = 0; i < list.Count; i++)
+//         {
+//             Change(ref list[i]);
+//         }
+//
+//         Console.Write(string.Join(' ', list));
+//     }
+//
+//     private static void Change(ref int value)
+//     {
+//         value *= 2;
+//     }
+// }
